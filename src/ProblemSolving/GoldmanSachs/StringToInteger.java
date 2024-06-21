74a9ab05dd0c36125953a9dcc7de4f259ccd8b61
parichaygupta <parichaygupta93@gmail.com>
2024-06-21 12:51:28 +0530

    Atomic Integer - String to Integer

--- src/ProblemSolving/GoldmanSachs/StringToInteger.java
+++ src/ProblemSolving/GoldmanSachs/StringToInteger.java
@@ -6,21 +6,46 @@ public class StringToInteger {
     *
     *   input: "24"
     *   output: 24
+    *
+    *   input: "-2348854"
+    *   output: -2348854
+    *
+    *   input: "24a"
+    *   output: 24
+    *
+    *   input: "a24"
+    *   output: -1
+    *
+    *   input: "24 5"
+    *   output: 24
+    *
+    *   input: -2445ane
+    *   output: -2445
     * */
     public static void main(String[] args) {
-        String s = "-2348854";
+        String s = "-233abcce";
         System.out.println(stringToInteger(s));
     }
 
     private static int stringToInteger(String s){
+        int i = 0;
+        int sign = 1;
         int result = 0;
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) < '0' || s.charAt(i) > '9'){
-                return -1;
+        if(s.length() == 0) return -1;
+        while(i < s.length() && s.charAt(i) == ' '){
+            i++;
+        }
+        if(i < s.length() && (s.charAt(i) == '-' || s.charAt(i) == '+')){
+            sign = s.charAt(i) == '-' ? -1 : 1;
+            i++;
+        }
+        while(i < s.length() && Character.isDigit(s.charAt(i))){
+            if(result > Integer.MAX_VALUE/10 || (result == Integer.MAX_VALUE/10 && s.charAt(i) - '0' > 7)){
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
             result = result * 10 + (s.charAt(i) - '0');
+            i++;
         }
-        return result;
-//        return Integer.parseInt(s);
+        return result * sign;
     }
 }
